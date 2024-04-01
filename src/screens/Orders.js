478472af85f3bae7ccd8169954e3144780f029e2
Stefano-Scozziero <scozzierostefano@gmail.com>
2024-04-01 16:40:25 -0300

    fix orders

--- src/screens/Orders.js
+++ src/screens/Orders.js
@@ -3,35 +3,31 @@ import OrderItem from '../components/presentational/OrderItem'
 import { useSelector } from 'react-redux'
 import { useGetOrdersQuery } from '../app/services/orders'
 import { OrientationContext } from '../utils/globals/context'
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import LoadingSpinner from '../components/presentational/LoadingSpinner'
 import Error from '../components/presentational/Error'
 
 const Orders = ({navigation}) => {
   const portrait = useContext(OrientationContext)
   const localId = useSelector((state) => state.auth.localId)
-  const {data, isError, error, isSuccess} = useGetOrdersQuery(localId)
-  const [info, setInfo] = useState(true)
-  const [loading, setLoading] = useState(true)
-  const [errorMessage, seterrorMessage] = useState("")
+  const {data:orders, isError, isLoading} = useGetOrdersQuery(localId)
 
- 
+  if(isLoading) return <LoadingSpinner/>
+  if(isError) return <Error message="¡Ups! Algo salió mal." textButton="Volver" onRetry={()=>navigation.goBack()}/>
 
-  useEffect(()=> {
-    if(isSuccess && data.length === 0) setInfo(false)
-    if(isSuccess && data) setLoading(false)
-    if(isError && error) seterrorMessage(error.error)
-  }, [data, isSuccess, isError, error])
-
-  if(!info) return <View style={[styles.containerText, !portrait && styles.containerTextLandScape]}><Text style={{fontSize:20}}>No hay órdenes</Text></View>
-  if(loading) return <LoadingSpinner/>
-  if(errorMessage) return <Error message="¡Ups! Algo salió mal." textButton="Volver" onRetry={()=>navigation.goBack()}/>
+  if (!orders || orders.length === 0) {
+    return (
+      <View style={[styles.containerText, !portrait && styles.containerTextLandScape]}>
+        <Text style={{fontSize:20}}>No hay órdenes</Text>
+      </View>
+    )
+  }
 
 
   return (
     <View style={[styles.flatList, !portrait && styles.flatListLandscape]}>
       <FlatList
-        data={data}
+        data={orders}
         keyExtractor={(item)=>item.id}
         renderItem={({item})=> <OrderItem order={item} portrait={portrait}/>}
       />
